1a5e9ff3e1844e437eb4543751bcbd181af93a68
David Prothero <david@prothero.com>
2019-02-11 15:22:17 -0800

    Making phones ring

--- src/hooks/init/twilio-api.js
+++ src/hooks/init/twilio-api.js
@@ -27,7 +27,11 @@ class TwilioRestApiPlugin extends Plugin {
 
   get topics() {
     // TODO: Hard-coding one resource/action for now.
-    const resource = this.apiBrowser.domains.api.versions.v2010.resources['/Accounts/{AccountSid}/Calls'];
+    const domainName = 'api';
+    const versionName = 'v2010';
+    const resource = this.apiBrowser.domains[domainName].versions[versionName].resources[
+      '/Accounts/{AccountSid}/Calls'
+    ];
     return [{ name: 'call', description: resource.description }];
   }
 
@@ -40,35 +44,88 @@ class TwilioRestApiPlugin extends Plugin {
     const commands = [];
 
     // TODO: Hard-coding one resource/action for now.
-    const resource = this.apiBrowser.domains.api.versions.v2010.resources['/Accounts/{AccountSid}/Calls'];
+    const domainName = 'api';
+    const versionName = 'v2010';
+    const currentPath = '/Accounts/{AccountSid}/Calls';
+    const resource = this.apiBrowser.domains[domainName].versions[versionName].resources[currentPath];
     const action = resource.actions.create;
     const cmd = class extends TwilioClientCommand {
       async run() {
         await super.run();
         const { flags } = this.parse(cmd);
 
+        // TODO: Possible extender event: "beforeValidateParameters"
+
         Object.keys(flags).forEach(key => {
-          if (Object.prototype.hasOwnProperty.call(cmd.flags[key], 'x-schema')) {
-            const schema = cmd.flags[key]['x-schema'];
+          if (Object.prototype.hasOwnProperty.call(cmd.flags[key], 'apiDetails')) {
+            const schema = cmd.flags[key].apiDetails.parameter.schema;
             // TODO: Run param validation for minLength, maxLength, and pattern
             this.logger.debug(`Schema for ${key}: ` + JSON.stringify(schema));
           }
         });
 
-        this.logger.info('TODO: Implement the API call based on provided flags.');
         this.logger.debug('Provided flags: ' + JSON.stringify(flags));
+
+        // TODO: Possible extender event: "afterValidateParameters"
+
+        // TODO: Possible extender event: "beforeInvokeApi"
+
+        // This converts a path like "/Accounts/{AccountSid}/Calls" to
+        // the Node.js object in the Twilio Helper library.
+        // Example: twilioClient.api.v2010.accounts('ACxxxx').calls
+        const helperVersion = this.twilioClient[domainName][versionName];
+        const pathNodes = currentPath.split('/');
+        pathNodes.splice(0, 1); // Remove first empty node
+        let endpoint = helperVersion;
+        pathNodes.forEach(pathNode => {
+          if (pathNode.startsWith('{')) {
+            const paramName = kebab(pathNode.replace(/[\{\}]/g, ''));
+            let value = '';
+            if (Object.hasOwnProperty.call(flags, paramName)) {
+              value = flags[paramName];
+            } else if (paramName === accountSidFlag) {
+              value = this.twilioClient.accountSid;
+            }
+            endpoint = endpoint(value);
+          } else {
+            // PascalCase to camelCase
+            pathNode = pathNode.substring(0, 1).toLowerCase() + pathNode.substring(1);
+            endpoint = endpoint[pathNode];
+          }
+        });
+
+        let response;
+
+        try {
+          response = await endpoint.create(flags);
+        } catch (error) {
+          this.logger.error(`Error response from Twilio: ${error.message}`);
+          this.logger.info(`See ${error.moreInfo} for more info.`);
+          this.exit(error.code);
+        }
+
+        // TODO: Figure out sane default output columns
+        this.output([response], this.flags.properties);
+
+        // TODO: Possible extender event: "afterInvokeApi"
       }
     };
 
     // Parameters
     const cmdFlags = {};
+    const isApi2010 = domainName === 'api' && versionName === 'v2010';
     action.parameters.forEach(p => {
       const flagName = kebab(p.name);
       const flagConfig = {
         description: p.description,
-        required: flagName === accountSidFlag ? false : p.required, // AccountSid not required, we can stuff from project
+        // AccountSid on api.v2010 not required, we can stuff from project
+        required: flagName === accountSidFlag && isApi2010 ? false : p.required,
         multiple: p.schema.type === 'array',
-        'x-schema': p.schema
+        apiDetails: {
+          parameter: p,
+          action: action,
+          resource: resource
+        }
       };
 
       let flagType = flags.string;
@@ -82,6 +139,11 @@ class TwilioRestApiPlugin extends Plugin {
       }
 
       cmdFlags[flagName] = flagType(flagConfig);
+    });
+
+    cmdFlags.properties = flags.string({
+      default: 'sid',
+      description: 'The properties you would like to display (JSON output always shows all properties)'
     });
 
     // Class statics
