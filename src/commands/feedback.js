96e43ea9e4ef285784094f43a722177f958cd05d
Ashley Roach <ashley.roach@sendgrid.com>
2019-04-16 07:23:53 -0600

    Add feedback command (#28)

---
+++ src/commands/feedback.js
@@ -0,0 +1,21 @@
+const chalk = require('chalk');
+const { Command } = require('@oclif/command');
+
+class Feedback extends Command {
+  async run() {
+    const link = chalk.bold('>>>>>  ') + chalk.underline.bold('https://bit.ly/twilio-cli-feedback') + chalk.bold('  <<<<<');
+    console.log();
+    console.log('***********************************************************************');
+    console.log('* Ahoy! Thank you for using twilio-cli!                               *');
+    console.log('* We are always striving to make twilio-cli better.                   *');
+    console.log('* Visit the link below to share your feedback with our team.          *');
+    console.log('*                                                                     *');
+    console.log(`*          ${link}           *`);
+    console.log('***********************************************************************');
+    console.log();
+  }
+}
+
+Feedback.description = 'Provide feedback to the CLI team';
+
+module.exports = Feedback;
---
+++ test/commands/feedback.test.js
@@ -0,0 +1,20 @@
+const { expect, test } = require('@twilio/cli-test');
+const { Config, ConfigData } = require('@twilio/cli-core').services.config;
+const Feedback = require('../../src/commands/feedback');
+
+describe('commands', () => {
+  describe('feedback', () => {
+    test
+      .do(ctx => {
+        ctx.userConfig = new ConfigData();
+      })
+      .twilioCliEnv(Config)
+      .stdout()
+      .stderr()
+      .twilioCommand(Feedback, [])
+      .it('runs feedback', ctx => {
+        expect(ctx.stdout).to.contain('Ahoy! Thank you for using twilio-cli!');
+        expect(ctx.stderr).to.equal('');
+      });
+  });
+});
