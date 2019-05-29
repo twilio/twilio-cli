76bcb1af9717b666a5e622efd4883cea364591ef
Elmer Thomas <elmer@thinkingserious.com>
2019-05-28 18:42:56 -0700

    Include a welcome getting started message on install

---
+++ welcome.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+console.log();
+console.log('*************************************************************************');
+console.log('*                                                                       *');
+console.log('* To get started, please create a project. Type the following to begin: *');
+console.log('*                                                                       *');
+console.log('* twilio login -p <PROJECT NAME>                                        *');
+console.log('*                                                                       *');
+console.log('*************************************************************************');
+console.log();
