b8a40a2cf62584bdd3d3e9ce921b7e96f70f5155
Elmer Thomas <elmer.thomas@sendgrid.com>
2019-06-03 13:03:06 -0700

    Show alternative and better spacing.

--- welcome.js
+++ welcome.js
@@ -4,7 +4,11 @@ console.log('*******************************************************************
 console.log('*                                                                       *');
 console.log('* To get started, please create a project. Type the following to begin: *');
 console.log('*                                                                       *');
-console.log('* twilio login -p <PROJECT NAME>                                        *');
+console.log('*     twilio login                                                      *');
+console.log('*                                                                       *');
+console.log('*     or                                                                *');
+console.log('*                                                                       *');
+console.log('*     twilio project:add <project name>                                 *');
 console.log('*                                                                       *');
 console.log('*************************************************************************');
 console.log();
