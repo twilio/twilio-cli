2873b2a19dfd33cbf2d46e4fd64372ac9186b9a5
David Prothero <david@prothero.com>
2019-06-04 11:46:10 -0700

    Refactor a bit, ignore empty plugin install error

--- src/commands/zork.js
+++ src/commands/zork.js
@@ -18,27 +18,44 @@ class Zork extends BaseCommand {
 
   async run() {
     await super.run();
+    let needToInstall = false;
 
     try {
       this.runZork();
     } catch (error) {
-      this.logger.warn('Standby, loading the dungeon...');
+      this.logger.debug('Error on initial attempt: ' + JSON.stringify(error));
+      needToInstall = true;
+    }
 
-      try {
-        this.plugins = this.plugins || new Plugins(this.config);
-        await this.plugins.install('zorkjs', { tag: 'latest', force: false });
-        this.runZork();
-      } catch (error) {
-        this.logger.debug(JSON.stringify(error));
-        this.logger.error('I don\'t know the word "zork".');
-        this.exit(1);
-      }
+    if (!needToInstall) return;
+
+    this.logger.warn('Standby, loading the dungeon...');
+    await this.installZork();
+
+    try {
+      this.runZork();
+    } catch (error) {
+      this.logger.debug('Error on second attempt: ' + JSON.stringify(error));
+      this.logger.error('I don\'t know the word "zork".');
+      this.exit(1);
     }
   }
 
   runZork() {
     const launchZork = this.findZork();
     launchZork();
+  }
+
+  async installZork() {
+    this.plugins = this.plugins || new Plugins(this.config);
+    try {
+      await this.plugins.install('zorkjs', { tag: 'latest', force: false });
+    } catch (error) {
+      // ignore plugin installation errors
+      if (Object.keys(error).length > 0) {
+        this.logger.debug('Error on plugin install: ' + JSON.stringify(error));
+      }
+    }
   }
 }
 
