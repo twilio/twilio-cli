9fc791bbbba61edcaf6a3822fa3d497f02e1a6ca
Jennifer Mah <jennifermah98@gmail.com>
2019-07-24 11:11:46 -0600

    Allow the user two chances to enter a project ID then quit.

--- test/commands/projects/add.test.js
+++ test/commands/projects/add.test.js
@@ -8,7 +8,7 @@ const helpMessages = require('../../../src/services/messaging/help-messages');
 describe('commands', () => {
   describe('projects', () => {
     describe('add', () => {
-      const addTest = (commandArgs = []) => test
+      const addTest = (commandArgs = [], projectID = 'default') => test
         .twilioFakeProject(ConfigData)
         .twilioCliEnv(Config)
         .twilioCreateCommand(ProjectsAdd, commandArgs)
@@ -19,7 +19,7 @@ describe('commands', () => {
           fakePrompt
             .onFirstCall()
             .resolves({
-              projectId: 'default'
+              projectId: projectID
             })
             .onSecondCall()
             .resolves({
@@ -55,6 +55,13 @@ describe('commands', () => {
           expect(ctx.stderr).to.contain(
             `See: https://www.twilio.com/console/runtime/api-keys/${constants.FAKE_API_KEY}`
           );
+        });
+
+      addTest([], '')
+        .do(ctx => ctx.testCmd.run())
+        .exit(1)
+        .it('fails for not entering a project ID', ctx => {
+          expect(ctx.stderr).to.contain('Shorthand identifier for your Twilio project was required');
         });
 
       addTest(['not-an-account-sid'])
