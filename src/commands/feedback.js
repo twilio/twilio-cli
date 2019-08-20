fd0a329bfb2914467774ef7ca675ea9447a491ec
Ashley Roach <ashley.roach@sendgrid.com>
2019-08-20 07:09:16 -0700

    Update feedback link (#103)

--- src/commands/feedback.js
+++ src/commands/feedback.js
@@ -4,14 +4,14 @@ const { Command } = require('@oclif/command');
 
 class Feedback extends Command {
   async run() {
-    const link = chalk.bold('>>>>>  ') + chalk.underline.bold('https://bit.ly/twilio-cli-feedback') + chalk.bold('  <<<<<');
+    const link = chalk.bold('>>>>>  ') + chalk.underline.bold('https://twil.io/twilio-cli-feedback') + chalk.bold('  <<<<<');
     console.log();
     console.log('***********************************************************************');
     console.log('* Ahoy! Thank you for using twilio-cli!                               *');
     console.log('* We are always striving to make twilio-cli better.                   *');
     console.log('* Visit the link below to share your feedback with our team.          *');
     console.log('*                                                                     *');
-    console.log(`*          ${link}           *`);
+    console.log(`*          ${link}          *`);
     console.log('***********************************************************************');
     console.log();
   }
