c44afa7756f6d2555c00dff763550090027a3e75
Sam Harrison <sharrison@twilio.com>
2020-06-08 11:08:41 -0500

    add plugin install mocking

--- test/hooks/command-not-found.test.js
+++ test/hooks/command-not-found.test.js
@@ -1,17 +1,26 @@
 const sinon = require('sinon');
 const inquirer = require('inquirer');
-const hookFunc = require('../../src/hooks/command-not-found');
+const proxyquire = require('proxyquire');
 const { expect, test } = require('@twilio/cli-test');
 
+function PluginsMock() {
+  this.install = sinon.stub();
+}
+
+const hookFunc = proxyquire('../../src/hooks/command-not-found', {
+  '@oclif/plugin-plugins': { default: PluginsMock }
+});
+
 const config = {
-  plugins: [{ name: '@twilio-labs/plugin-watch' }]
+  plugins: [{ name: '@twilio-labs/plugin-watch' }],
+  runHook: sinon.stub()
 };
 
 describe('hooks', () => {
   describe('command-not-found', () => {
     before(() => {
       inquirer._prompt = inquirer.prompt;
-      inquirer.prompt = sinon.stub().resolves({ continue: false });
+      inquirer.prompt = sinon.stub().resolves({ install: true });
     });
 
     after(() => {
@@ -19,12 +28,14 @@ describe('hooks', () => {
       delete inquirer._prompt;
     });
 
-    test.stderr().it('warns for non-installed plugin topics', async ctx => {
+    test.stderr().it('warns and installs non-installed plugin topics', async ctx => {
+      ctx.exit = sinon.stub().resolves(1);
       await hookFunc.call(ctx, { id: 'serverless', config });
       expect(ctx.stderr).to.contain('not installed');
     });
 
-    test.stderr().it('warns for non-installed plugins commands', async ctx => {
+    test.stderr().it('warns and install non-installed plugins commands', async ctx => {
+      ctx.exit = sinon.stub().resolves(1);
       await hookFunc.call(ctx, { id: 'serverless:init', config });
       expect(ctx.stderr).to.contain('not installed');
     });
