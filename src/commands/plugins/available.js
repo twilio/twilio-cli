e62a9237febc416aaec38ad9daa163a8529d6c01
Sam Harrison <sharrison@twilio.com>
2020-06-11 17:02:15 -0500

    feat: add 'plugins:available' command that lists trusted, uninstalled plugins
    Fixes #117

---
+++ src/commands/plugins/available.js
@@ -0,0 +1,16 @@
+const { BaseCommand } = require('@twilio/cli-core').baseCommands;
+const { getAvailablePlugins } = require('../../services/plugins');
+
+class PluginsAvailable extends BaseCommand {
+  async run() {
+    await super.run();
+
+    getAvailablePlugins(this.config)
+      .forEach(pluginName => this.logger.info(pluginName));
+  }
+}
+
+PluginsAvailable.description = 'list available plugins for installation';
+PluginsAvailable.flags = null;
+
+module.exports = PluginsAvailable;
---
+++ src/services/plugins.js
@@ -0,0 +1,36 @@
+const ALLOWED_ORGS = [
+  '@twilio/',
+  '@twilio-labs/',
+  '@dabblelab/plugin-autopilot'
+];
+
+const PLUGIN_COMMANDS = {
+  '@twilio-labs/plugin-flex': ['flex'],
+  '@twilio-labs/plugin-rtc': ['rtc'],
+  '@twilio-labs/plugin-serverless': ['serverless'],
+  '@twilio-labs/plugin-token': ['token'],
+  '@twilio-labs/plugin-watch': ['watch'],
+  '@dabblelab/plugin-autopilot': ['autopilot']
+};
+
+exports.isTwilioPlugin = pluginName => {
+  if (pluginName === undefined) {
+    return false;
+  }
+  return ALLOWED_ORGS.find(org => pluginName.startsWith(org));
+};
+
+exports.isPluginInstalled = (config, pluginName) => {
+  return config.plugins.find(p => p.name === pluginName);
+};
+
+exports.getSupportedPlugin = commandId => {
+  return Object.keys(PLUGIN_COMMANDS).find(plugin => {
+    return PLUGIN_COMMANDS[plugin].find(command => commandId === command || commandId.startsWith(command + ':'));
+  });
+};
+
+exports.getAvailablePlugins = config => {
+  return Object.keys(PLUGIN_COMMANDS)
+    .filter(pluginName => !exports.isPluginInstalled(config, pluginName));
+};
