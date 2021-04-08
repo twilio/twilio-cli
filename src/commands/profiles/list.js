f35386210febcbe7b8ef0f9bd1fadb07826b8ed0
Anuj <abadhwar@twilio.com>
2021-04-08 17:25:23 +0530

    Fix profile list

--- src/commands/profiles/list.js
+++ src/commands/profiles/list.js
@@ -4,12 +4,22 @@ const { BaseCommand } = require('@twilio/cli-core').baseCommands;
 class ProfilesList extends BaseCommand {
   async run() {
     await super.run();
+    const envProfile = this.userConfig.getProfileFromEnvironment();
+    let strippedEnvProfile = undefined;
+    if (typeof envProfile !== 'undefined') {
+      strippedEnvProfile = {
+        id: '[env]',
+        accountSid: envProfile.accountSid,
+        region: envProfile.region,
+      };
+      this.userConfig.profiles.push(strippedEnvProfile);
+    }
     if (this.userConfig.profiles.length > 0) {
       // If none of the profiles have a region, delete it from all of them so it doesn't show up in the output.
       if (!this.userConfig.profiles.some((p) => p.region)) {
         this.userConfig.profiles.forEach((p) => delete p.region);
       }
-      const activeProfile = this.userConfig.getActiveProfile();
+      const activeProfile = typeof envProfile === 'undefined' ? this.userConfig.getActiveProfile() : strippedEnvProfile;
       this.userConfig.profiles.forEach((p) => {
         if (p.id === activeProfile.id) {
           p.active = true;
