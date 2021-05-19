13173be624ddb6c69aef239f59ae0b487e42b874
Shwetha Radhakrishna <shwetha-manvinkurke@users.noreply.github.com>
2021-05-19 15:04:57 -0700

    fix: conform to eslint guidelines (#254)
    * chore: conform to eslint guidelines

--- src/services/email-utility.js
+++ src/services/email-utility.js
@@ -1,6 +1,8 @@
 function validateEmail(email) {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const verdict = re.test(email.toLowerCase());
   return verdict;
 }
+
 module.exports = { validateEmail };
