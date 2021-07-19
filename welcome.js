5e564e729cc6b51afbeef1ff57ef89756ebc1751
Anuj Badhwar <abadhwar@twilio.com>
2021-07-19 10:55:01 +0530

    chore: change post install messaging behaviour (#269)

--- welcome.js
+++ welcome.js
@@ -1,14 +1,3 @@
-/* eslint-disable no-console */
-console.log();
-console.log('*************************************************************************');
-console.log('*                                                                       *');
-console.log('* To get started, please create a twilio-cli profile:                   *');
-console.log('*                                                                       *');
-console.log('*     twilio login                                                      *');
-console.log('*                                                                       *');
-console.log('*     OR                                                                *');
-console.log('*                                                                       *');
-console.log('*     twilio profiles:create                                            *');
-console.log('*                                                                       *');
-console.log('*************************************************************************');
-console.log();
+const { PostInstallDisplayManager } = require('./src/services/post-install-utility');
+
+new PostInstallDisplayManager().displayMessage();
