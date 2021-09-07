c9ba162f2ee88d9ff24bf4543e709647e3ee3ead
lakshmiravali <lakshmiravali.rimmalapudi@gmail.com>
2021-09-07 10:27:32 +0530

    Refactoring the code and addresed the review comments

--- .github/scripts/commit-api-spec-change-log.sh
+++ .github/scripts/commit-api-spec-change-log.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 echo "Running update changelog script"
 changeLog="$1"
+versionType="$2"
 echo "$changeLog"
-node scripts/update-change-log.js "$changeLog"
+node .github/scripts/update-change-log.js "$changeLog"
 echo "Git configurations"
 git config --global user.email "team_interfaces+github@twilio.com"
 git config --global user.name "twilio-dx"
@@ -12,14 +13,14 @@ if [ -n "$(git status --porcelain)" ]; then
   echo "Current branch: $branch"
   echo "There are changes to commit.";
   commitMessage=''
-  if [ "$2" == 0 ] || [ "$2" == 1 ]
+  if [ "$versionType" == 0 ] || [ "$versionType" == 1 ]
   then
     commitMessage='feat: Updated api definitions changelog in CHANGES.md'
-  elif [ "$2" == 2 ]
+  elif [ "$versionType" == 2 ]
   then
     commitMessage='fix: Updated api definitions changelog in CHANGES.md'
   else
-    echo "Invalid versionType: $2";
+    echo "Invalid versionType: $versionType";
     exit
   fi
   echo "Commit message:$commitMessage"
