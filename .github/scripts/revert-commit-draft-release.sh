afd45fb9cdc68261fa3d2d99670eb22da6ac55c3
shrutiburman <87537688+shrutiburman@users.noreply.github.com>
2021-11-17 16:29:52 +0530

    Review comments modifications

--- .github/scripts/revert-commit-draft-release.sh
+++ .github/scripts/revert-commit-draft-release.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 echo "Running revert last commit script"
 echo $1
-echo "Last commit to revert :"
+echo "Reverting commit:"
 echo $(git rev-parse --verify HEAD)
 echo "Git configurations :"
 git config --global user.email "team_interfaces+github@twilio.com"
 git config --global user.name "twilio-dx"
+git revert $(git rev-parse --verify HEAD)
+git push origin refs/heads/$1:refs/heads/$1
+echo "Reverting commit:"
+echo $(git rev-parse --verify HEAD)
 git revert $(git rev-parse --verify HEAD)
 git push origin refs/heads/$1:refs/heads/$1
 
