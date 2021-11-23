3065a8a046c7872704bb2f01c3032734a88d38e4
sburman <sburman@twilio.com>
2021-11-23 20:45:48 +0530

    Review comments to add wait for workflow completion

--- .github/scripts/revert-commit-draft-release.sh
+++ .github/scripts/revert-commit-draft-release.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 echo "Running revert last commit script"
-echo $1
-echo "Reverting commit:"
-echo $(git rev-parse --verify HEAD)
+branch="$1"
 echo "Git configurations :"
 git config --global user.email "team_interfaces+github@twilio.com"
 git config --global user.name "twilio-dx"
-git revert $(git rev-parse --verify HEAD)
-git push origin refs/heads/$1:refs/heads/$1
-echo "Reverting commit:"
-echo $(git rev-parse --verify HEAD)
-git revert $(git rev-parse --verify HEAD)
-git push origin refs/heads/$1:refs/heads/$1
-
-
+echo $branch
+nCommit=$(git rev-parse --verify HEAD)
+nCommitAuthor=$(git log -n 1 --pretty=format:'%an' $nCommit)
+if [[ $nCommitAuthor == *"semantic-release-bot"* ]]; then
+  echo "Reverting commit by semantic release bot:"
+  echo $(git rev-parse --verify HEAD)
+  git revert $(git rev-parse --verify HEAD)
+  git push origin refs/heads/$1:refs/heads/$1
+fi
+nCommit=$(git rev-parse --verify HEAD)
+nCommitAuthor=$(git log -n 1 --pretty=format:'%an' $nCommit)
+if [[ $nCommitAuthor == *"twilio-dx"* ]]; then
+  echo "Reverting commit for oai spec:"
+  echo $(git rev-parse --verify HEAD)
+  git revert $(git rev-parse --verify HEAD)
+  git push origin refs/heads/$1:refs/heads/$1
+fi
 
