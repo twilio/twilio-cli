2123d34b064760fbf3cdf698e742c387bdfcca01
shrutiburman <87537688+shrutiburman@users.noreply.github.com>
2021-12-29 00:29:30 +0530

    Fixing failed sanity test cases

---
+++ .github/scripts/cmds-to-test.sh
@@ -0,0 +1,11 @@
+echo "Testing these commands for sanity of installation methods"
+echo "1. twilio --version"
+twilio --version
+echo "2. twilio api:core:addresses:list --help"
+twilio api:core:addresses:list --help
+echo "3. twilio profiles:list"
+twilio profiles:list
+echo "4. twilio help"
+twilio help
+echo "5. twilio api:core:calls --help"
+twilio api:core:calls --help
