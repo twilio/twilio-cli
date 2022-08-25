9a95b91a11a95c6c34ee52171e324c70f9c987d8
kridai <kridaigoswami@gmail.com>
2022-08-25 10:51:13 +0530

    chore: update macos failing script (#453)
    Co-authored-by: shrutiburman <87537688+shrutiburman@users.noreply.github.com>

--- .github/scripts/pack-debian-apt.js
+++ .github/scripts/pack-debian-apt.js
@@ -86,19 +86,20 @@ PATH=$PATH:$PWD/bin eval $(PATH=$PATH:$PWD/bin node -p "require('./package').scr
     try {
       // fetch existing Packages file which needs to be modified for new version
       await qq.x(`aws s3 cp s3://${pjson.oclif.update.s3.bucket}/apt/Packages Packages`, {cwd: dist, reject: false});
-      const content = readFileSync("Packages");
-      fs.readFile('Packages', function (err, data) {
-        if(err) throw err;
-        // check if version already exists
-        if(data.includes(`Version: ${debVersion}`)){
-          console.log('the version ${debVersion}` is already available');
-          return
-        }
-      });
+      const content = fs.readFileSync(`${dist}/Packages`);
     }
     catch(error) {
       console.log(`Cannot retrieve Packages file due to error: ${error} `);
+      throw error;
     }
+    fs.readFile(`${dist}/Packages`, function (err, data) {
+    if(err) throw err;
+    // check if version already exists
+    if(data.includes(`Version: ${debVersion}`)){
+      console.log(`the version ${debVersion} is already available`);
+      throw new Error('verion already exists');
+      }
+    });
     for (const a of arch) {
       await build(a);
       await qq.x(`apt-ftparchive packages ${debArch(a)}/ >> Packages`, {cwd: dist});
