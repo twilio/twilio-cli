eabeb84c6bb6bb31005ef6f60a0c443c184da776
Dominic <solaveritasteliberum@gmail.com>
2023-03-07 11:20:18 -0800

    Use dpkg-deb instead of dpkg to build .deb package

--- .github/scripts/pack-debian-apt.js
+++ .github/scripts/pack-debian-apt.js
@@ -82,7 +82,7 @@ PATH=$PATH:$PWD/bin eval $(PATH=$PATH:$PWD/bin node -p "require('./package').scr
       await qq.chmod([workspace, 'DEBIAN/postinst'], 0o755);
       await qq.x(`ln -s "../lib/${config.dirname}/bin/${config.bin}" "${workspace}/usr/bin/${pjson.oclif.bin}"`);
       // Use xz compression until Debian supports zstd (see issue #482)
-      await qq.x(`dpkg -Zxz --build "${workspace}" "${qq.join(dist, debArch(arch), `${versionedDebBase}.deb`)}"`);
+      await qq.x(`dpkg-deb -Zxz --build "${workspace}" "${qq.join(dist, debArch(arch), `${versionedDebBase}.deb`)}"`);
     }
     try {
       // fetch existing Packages file which needs to be modified for new version
