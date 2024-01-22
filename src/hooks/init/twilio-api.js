17a7fea84fe45af156704302d3575dd5a45ce2c8
Shubham <tiwarishubham635@gmail.com>
2024-01-22 14:26:30 +0530

    chore: skipping healthcheck api

--- src/hooks/init/twilio-api.js
+++ src/hooks/init/twilio-api.js
@@ -61,6 +61,7 @@ class TwilioRestApiPlugin extends Plugin {
     };
 
     Object.keys(actionDefinition.domain.paths).forEach((pathName) => {
+      if(pathName === '/healthcheck') return;
       const versionName = pathName.split('/')[1];
       const shortVersion = versionName.replace(/v/g, '');
 
